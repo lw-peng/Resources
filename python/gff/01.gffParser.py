4b52724263febed01886f042269118f824fd19e6
lab-zy <143925439+lab-zy@users.noreply.github.com>
2024-03-22 09:59:02 +0800

    Update 01.gffParser.py

--- python/gff/01.gffParser.py
+++ python/gff/01.gffParser.py
@@ -5,7 +5,7 @@ import gzip
 python process_gff.py -i /path/to/your.gff or your.gff.gz -o /path/to/output.txt
 """
 
-def parse_gff(gff_file_path):
+def parse_gff(gff_file_path, genome_name):
     genes_info = []
 
     # Determine if the file is gzipped based on its extension
@@ -31,7 +31,7 @@ def parse_gff(gff_file_path):
                     gene_length = end_position - start_position + 1
                     expanded_start_position = start_position - 500
                     expanded_end_position = end_position + 500
-                    genes_info.append((chromosome, start_position, end_position, gene_name, gene_length, strand, f"{chromosome}:{expanded_start_position}..{expanded_end_position}"))
+                    genes_info.append((gene_name, genome_name, chromosome, start_position, end_position, gene_length, strand, f"{chromosome}:{expanded_start_position}..{expanded_end_position}"))
 
     return genes_info
 
@@ -39,14 +39,15 @@ def main():
     parser = argparse.ArgumentParser(description='Parse GFF or GFF.GZ file and extract gene information.')
     parser.add_argument('-i', '--input_gff', required=True, help='Path to the GFF or GFF.GZ file.')
     parser.add_argument('-o', '--output_file', required=True, help='Path to the output TXT file.')
+    parser.add_argument('-g', '--genome_name', type=str, required=True, help='Name of the genome')
 
     args = parser.parse_args()
 
-    genes_info = parse_gff(args.input_gff)
+    genes_info = parse_gff(args.input_gff, args.genome_name)
 
     with open(args.output_file, 'w') as out_file:
         for info in genes_info:
-            out_file.write(f"{info[0]}\t{info[1]}\t{info[2]}\t{info[3]}\t{info[4]}\t{info[5]}\t{info[6]}\n")
+            out_file.write(f"{info[0]}\t{info[1]}\t{info[2]}\t{info[3]}\t{info[4]}\t{info[5]}\t{info[6]}\t{info[7]}\n")
 
 if __name__ == '__main__':
     main()
